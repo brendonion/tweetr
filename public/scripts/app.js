e3acc86d00457e18fa27a3fd91562cb52fdea970
Brendan Walker <brenny_walker@hotmail.com>
2017-03-22 19:48:20 +0000

    added rendering of hard-coded tweets database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,26 +14,109 @@ var tweetData = {
   "created_at": 1461116232227
 }
 
+var tweets = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
 
 function createTweetElement(tweetData){
-    let name = tweetData.user.name;
-    let avatar = tweetData.user.avatars.regular;
-    let handle = tweetData.user.handle;
-    let content = tweetData.user.content;
-    let time = tweetData.user.created_at;
+  const name = tweetData.user.name;
+  const avatar = tweetData.user.avatars.small;
+  const handle = tweetData.user.handle;
+  const content = tweetData.content.text;
+  const time = tweetData.created_at;
 
-    let headerName = $("header h2").append(name);
-    console.log(headerName);
+  let $tweet = $("<article>").addClass("tweets")
+    .append($('<header>')
+      .append($('<img>').attr('src', avatar))
+      .append($('<h2>').text(name))
+      .append($('<h5>').text(handle))
+    )
+    .append($('<div>').addClass('tweet-body')
+      .append($('<p>').text(content))
+    )
+    .append($('<footer>')
+      .append($('<span>').text(time))
+      .append($('<div>').addClass('icons')
+        .append($('<i>').addClass('fa fa-flag').attr('aria-hidden', 'true'))
+        .append($('<i>').addClass('fa fa-retweet').attr('aria-hidden', 'true'))
+        .append($('<i>').addClass('fa fa-heart').attr('aria-hidden', 'true'))
+        )
+      );
+    return $tweet;
+  }
+
+
+function renderTweets(tweets) {
+  tweets.forEach(function(tweet){
+    let $tweet = createTweetElement(tweet);
+    $('#tweets-container').append($tweet);
+  })
 }
 
+// $('.new-tweet form').on('submit', function(event){
+//   event.preventDefault();
+//   $.ajax({
+//     url: '/tweets/',
+//     method: 'POST',
+//     data: {
+//       type: $('textarea').serlialize().val();
+//     }
+//   })
+//   console.log('cheers');
+
+// })
 
 
 
 
-var $tweet = createTweetElement(tweetData);
+$(document).ready(function() {
+  console.log('these are the tweets:', tweets);
+  renderTweets(tweets);
+});
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-//$('#tweets-container').append($tweet); // to add it to the page so we can make sure
-                                       //it's got all the right elements, classes, etc.
+
 
