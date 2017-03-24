232f4e38c12bd606bb6e7128e5c716908b54db63
Brendan Walker <brenny_walker@hotmail.com>
2017-03-24 00:23:39 +0000

    Replaced in memory database with a mongoDB database. Fixed some styling in the header. Cleaned up code as well.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,55 +14,11 @@ var tweetData = {
   "created_at": 1461116232227
 }
 
-var tweets = [
-  // {
-  //   "user": {
-  //     "name": "Newton",
-  //     "avatars": {
-  //       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-  //       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-  //       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-  //     },
-  //     "handle": "@SirIsaac"
-  //   },
-  //   "content": {
-  //     "text": "If I have seen further it is by standing on the shoulders of giants"
-  //   },
-  //   "created_at": 1461116232227
-  // },
-  // {
-  //   "user": {
-  //     "name": "Descartes",
-  //     "avatars": {
-  //       "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-  //       "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-  //       "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-  //     },
-  //     "handle": "@rd" },
-  //   "content": {
-  //     "text": "Je pense , donc je suis"
-  //   },
-  //   "created_at": 1461113959088
-  // },
-  // {
-  //   "user": {
-  //     "name": "Johann von Goethe",
-  //     "avatars": {
-  //       "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-  //       "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-  //       "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-  //     },
-  //     "handle": "@johann49"
-  //   },
-  //   "content": {
-  //     "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-  //   },
-  //   "created_at": 1461113796368
-  // }
-];
+var tweets = [];
 
 
 function createTweetElement(tweetData){
+
   const name = tweetData.user.name;
   const avatar = tweetData.user.avatars.small;
   const handle = tweetData.user.handle;
@@ -79,7 +35,7 @@ function createTweetElement(tweetData){
       .append($('<p>').text(content))
     )
     .append($('<footer>')
-      .append($('<span>').text(time))
+      .append($('<span>').text(new Date(time).toLocaleString()))
       .append($('<div>').addClass('icons')
         .append($('<i>').addClass('fa fa-flag').attr('aria-hidden', 'true'))
         .append($('<i>').addClass('fa fa-retweet').attr('aria-hidden', 'true'))
@@ -101,10 +57,13 @@ function renderTweets(tweets) {
 $(document).ready(function() {
 
   $('.new-tweet form').on('submit', function(event){
+
     event.preventDefault();
-    let input = $('.new-tweet form textarea').val();
-    let contents = $('.new-tweet form textarea'); //may need to serialize later
+
+    let contents = $('.new-tweet form textarea');
+    let input = contents.val();
     let errorMessage = $('.new-tweet h4');
+
     if (input.length == 0){
       errorMessage.html('Too short!');
     } else if (input.length > 140) {
@@ -146,8 +105,6 @@ $(document).ready(function() {
       $('.new-tweet').slideUp('fast');
     }
   });
-
-
 
 });
 
